4bcef33fce2bbde28fbce9eeb304c3801b70a3d5
kuris13 <duddn4646@gmail.com>
2020-08-28 13:57:44 +0900

    bird finish

--- Practice/win32api/Win32_API/birdScene.cpp
+++ Practice/win32api/Win32_API/birdScene.cpp
@@ -1,6 +1,8 @@
 #include "setDefine.h"
 #include "birdScene.h"
 
+
+
 HRESULT birdScene::init()
 {
 	gameStop = true;
@@ -8,19 +10,30 @@ HRESULT birdScene::init()
 	IMAGEMANAGER->addImage("ufo", "Images/ufo.bmp", 58, 40, true, RGB(255, 255, 255));
 	IMAGEMANAGER->addImage("적", "Images/enemy.bmp", 100, 100, true, RGB(255, 0, 255));
 
-	enemy[0] = RectMakeCenter(WINSIZE_X - 150, 50, 100, 100);
-	enemy[1] = RectMakeCenter(WINSIZE_X - 150, 150, 100, 100);
-	enemy[2] = RectMakeCenter(WINSIZE_X - 150, WINSIZE_Y - 200, 100, 100);
-	enemy[3] = RectMakeCenter(WINSIZE_X - 150, WINSIZE_Y-100, 100, 100);
-
-	player = RectMakeCenter(200, WINSIZE_Y / 2, 58, 40);
-
+	
+	gameInit();
 
 	return S_OK;
 }
 
+void birdScene::gameInit()
+{
+	score = 0;
+	enemy[0] = RectMakeCenter(WINSIZE_X - 150, 50, 100, 100);
+	enemy[1] = RectMakeCenter(WINSIZE_X - 150, 150, 100, 100);
+	enemy[2] = RectMakeCenter(WINSIZE_X - 150, 250, 100, 100);
+
+	enemy[3] = RectMakeCenter(WINSIZE_X - 150, 350, 100, 100);
+	enemy[4] = RectMakeCenter(WINSIZE_X - 150, 450, 100, 100);
+	enemy[5] = RectMakeCenter(WINSIZE_X - 150, 550, 100, 100);
+	enemy[6] = RectMakeCenter(WINSIZE_X - 150, 650, 100, 100);
+	player = RectMakeCenter(200, WINSIZE_Y / 2, 58, 40);
+}
+
 void birdScene::release()
 {
+	
+
 }
 
 void birdScene::update()
@@ -33,7 +46,7 @@ void birdScene::update()
 
 
 		RECT temp;
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 7; i++)
 		{
 			if (IntersectRect(&temp, &player, &enemy[i]))
 			{
@@ -62,8 +75,9 @@ void birdScene::update()
 			++jC;
 		}
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 7; i++)
 		{
+			if (i == randEnemy || i == randEnemy + 1) continue;
 			enemy[i].left -= 6;
 			enemy[i].right -= 6;
 
@@ -71,13 +85,15 @@ void birdScene::update()
 
 		if (enemy[0].right < 0)
 		{
-			for (int i = 0; i < 4; i++)
+			for (int i = 0; i < 7; i++)
 			{
 				enemy[i].left = WINSIZE_X;
 				enemy[i].right = WINSIZE_X + 100;
 
+				
+				randEnemy = RND->getFromIntTo(1, 5);
 			}
-
+			++score;
 		}
 
 
@@ -93,8 +109,7 @@ void birdScene::update()
 	if (KEYMANAGER->isOnceKeyDown(VK_UP))
 	{
 		gameStop = true;
-
-		player = RectMakeCenter(200, WINSIZE_Y / 2, 58, 40);
+		gameInit();
 	}
 }
 
@@ -104,9 +119,24 @@ void birdScene::render()
 
 	IMAGEMANAGER->render("ufo", getMemDC(),player.left,player.top);
 
-	IMAGEMANAGER->render("적", getMemDC(), enemy[0].left, enemy[0].top);
-	IMAGEMANAGER->render("적", getMemDC(), enemy[1].left, enemy[1].top);
-	IMAGEMANAGER->render("적", getMemDC(), enemy[2].left, enemy[2].top);
-	IMAGEMANAGER->render("적", getMemDC(), enemy[3].left, enemy[3].top);
+
+	for (int i = 0; i < 7; i++)
+	{
+
+		if (i == randEnemy || i == randEnemy + 1) continue;
+		IMAGEMANAGER->render("적", getMemDC(), enemy[i].left, enemy[i].top);
+	}
+
+	
+	wsprintf(str,"scroe : %d",score);
+	wsprintf(str2, "다시 하실려면 위쪽 화살표를 눌러주세요");
+
+
+	TextOut(getMemDC(), WINSIZE_X - 200, 50, str, strlen(str));
+
+	if (gameStop == false)
+	{
+		TextOut(getMemDC(), WINSIZE_X/2, WINSIZE_Y /2, str2, strlen(str2));
+	}
 
 }
--- Practice/win32api/Win32_API/birdScene.h
+++ Practice/win32api/Win32_API/birdScene.h
@@ -2,19 +2,25 @@
 #include "gameNode.h"
 class birdScene : public gameNode
 {
-
+	
 private:
 
 	RECT background;
-	RECT enemy[4];
+	RECT enemy[7];
 	RECT player;
 	int pow = 0;
 	int _offsetX1 = 0, _offsetY1 = 0;
 	bool isJump = false;
 	int jC = 0;
 	
+	int score=0;
+	char str[128];
+	char str2[128];
+	int randEnemy=3;
+
 	bool gameStop = false;;
 public:
+	void gameInit();
 	HRESULT init();
 	void release();
 	void update();
